769b0e99ae69965054d4ecb186ecb1306eeb9c9f
shengang1978 <shengang1978@hotmail.com>
2015-06-23 13:02:03 +0800

    up2

--- main/social/src/main/java/com/divx/service/model/CreatePartyResponse.java
+++ main/social/src/main/java/com/divx/service/model/CreatePartyResponse.java
@@ -1,5 +1,13 @@
 package com.divx.service.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "CreatePartyResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CreatePartyResponse")
 public class CreatePartyResponse extends ServiceResponse {
 	private int partyId;
 
